5af1abd048c7413a8d50bfd2ab8c21b50bfb16cc
Damian Domin <damian.domin334@gmail.com>
2025-04-29 17:27:20 +0200

    Add color palete for dark mode and darkmode selector for primevue

--- src/main.js
+++ src/main.js
@@ -11,16 +11,68 @@ import 'primeicons/primeicons.css'
 
 import Aura from '@primeuix/themes/aura';
 
+import { definePreset } from '@primeuix/themes'
+
 
 const app = createApp(App)
 
+const MyPreset = definePreset(Aura, {
+    semantic: {
+        colorScheme: {
+            light: {
+                surface: {
+                    0: '#ffffff',
+                    50: '{zinc.50}',
+                    100: '{zinc.100}',
+                    200: '{zinc.200}',
+                    300: '{zinc.300}',
+                    400: '{zinc.400}',
+                    500: '{zinc.500}',
+                    600: '{zinc.600}',
+                    700: '{zinc.700}',
+                    800: '{zinc.800}',
+                    900: '{zinc.900}',
+                    950: '{zinc.950}'
+                }
+            },
+            dark: {
+                surface: {
+                    0: '#ffffff',
+                    50: '{gray.50}',
+                    100: '{gray.100}',
+                    200: '{gray.200}',
+                    300: '{gray.300}',
+                    400: '{gray.400}',
+                    500: '{gray.500}',
+                    600: '{gray.600}',
+                    700: '{gray.700}',
+                    800: '{gray.800}',
+                    900: '{gray.900}',
+                    950: '{gray.950}'
+                }
+            }
+        }
+    },
+    components: {
+        divider: {
+            colorScheme: {
+                dark: {
+                    border: {
+                        color: '{gray.600}'
+                    }
+                }
+            }
+        }
+    }
+});
 
 app.use(createPinia())
 app.use(router)
 app.use(PrimeVue, {
     theme: {
-        preset: Aura,
+        preset: MyPreset,
         options: {
+            darkModeSelector: '.my-app-dark',
             cssLayer: {
                 name: 'primevue',
                 order: 'theme, base, primevue'
