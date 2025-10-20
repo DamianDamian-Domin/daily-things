f0ab36449d70d48bc612d93a52a54b5605b147ac
Damian Domin <damian.domin334@gmail.com>
2025-10-20 22:15:34 +0200

    Add navbar dropdown + Add logout action call

--- src/components/navbar/NavBar.vue
+++ src/components/navbar/NavBar.vue
@@ -9,19 +9,32 @@
             <Button class="w-12 h-12" text v-if="isDarkMode" icon="pi pi-sun" severity="secondary" @click="toggleDarkMode()" />
             <Button class="w-12 h-12" text v-else icon="pi pi-moon" severity="secondary" @click="toggleDarkMode()" />
             <Button class="w-12 h-12" rounded icon="pi pi-ellipsis-v" severity="secondary"></Button>
-            <Button class="w-12 h-12" rounded label="DD" severity="primary"></Button>
+            <Button @click="toggle" class="w-12 h-12" rounded label="DD" severity="primary"></Button>
+            <Popover ref="op">
+                <div class="flex flex-col gap-2 w-[12rem]">
+                    <Button size="small" text label="Profile" icon="pi pi-user" severity="secondary" />
+                    <Button size="small" text label="Settings" icon="pi pi-cog" severity="secondary" />
+                    <Button @click="authStore.logout()" size="small" text label="Logout" icon="pi pi-sign-out" severity="danger" />
+                </div>
+            </Popover>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
-import { onMounted } from 'vue';
+import { ref, onMounted } from 'vue';
 import Button from 'primevue/button';
+import Popover from 'primevue/popover';
 import { useCommonStore } from '@/stores/common'
 import { storeToRefs } from 'pinia';
+import { useAuthStore } from '@/stores/auth';
 
 const commonStore = useCommonStore()
 const { isDarkMode } = storeToRefs(commonStore)
+
+const authStore = useAuthStore()
+
+const op = ref();
 
 const toggleDarkMode = () => {
 
@@ -34,6 +47,10 @@ const toggleDarkMode = () => {
         localStorage.setItem('theme', 'light')
     }
 
+}
+
+const toggle = (event: MouseEvent) => {
+    op.value.toggle(event);
 }
 
 onMounted(() => {
